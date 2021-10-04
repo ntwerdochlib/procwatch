462fda235e3a19083b6f6548ba3de500087dfc75
Nicholas Twerdochlib <ntwerdochlib@bomgar.com>
2021-10-04 12:01:26 -0400

    Add ModifyHandleInEpoll() Add RemoveHandleFromEpoll()

--- util/utils.h
+++ util/utils.h
@@ -50,5 +50,31 @@ namespace pw_util {
       throw std::runtime_error(std::string("Failed to add handle to epoll: ").append(std::to_string(handle) + " - " + strerror(errno)));
     }
   }
+  static void ModifyHandleInEpoll(Handle& epollHandle, const int& handle, uint32_t events) {
+    epoll_event ev {
+      .events = events,
+      .data = {
+        .fd = handle
+      }
+    };
+
+    auto r = epoll_ctl(epollHandle, EPOLL_CTL_MOD, handle, &ev);
+    if (r == -1) {
+      throw std::runtime_error(std::string("Failed to delete handle from epoll: ").append(std::to_string(handle) + " - " + strerror(errno)));
+    }
 }
+  static void RemoveHandleFromEpoll(Handle& epollHandle, const int& handle, uint32_t events) {
+    epoll_event ev {
+      .events = events,
+      .data = {
+        .fd = handle
+      }
+    };
+
+    auto r = epoll_ctl(epollHandle, EPOLL_CTL_DEL, handle, &ev);
+    if (r == -1) {
+      throw std::runtime_error(std::string("Failed to delete handle from epoll: ").append(std::to_string(handle) + " - " + strerror(errno)));
+    }
+  }
+} // end namespace pw_util
 #endif // PW_UTILS_H
